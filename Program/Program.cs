1853ef1a14909b09b35abdb15ed5cdcb850ef1f8
Vasya comp <vasja11420@gmail.com>
2023-03-09 20:51:21 +0500

    Создали функцию введения значений массива строк

---
+++ Program/Program.cs
@@ -0,0 +1,19 @@
+﻿
+string[] FillArray()
+{
+    Console.Write("Введите длинну массива: ");
+    int size = int.Parse(Console.ReadLine()!);
+
+    string[] result = new string[size];
+    string value = string.Empty;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите {i + 1}-е значение массива: ");
+        value = Console.ReadLine()!;
+        result[i] = value;
+    }
+    return result;
+
+}
+
+
